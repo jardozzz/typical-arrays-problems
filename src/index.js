62d788cb5f73dba5b77480c8beabd4eb009bbe6e
Jardozzz <jardozzz@gmail.com>
2021-03-15 13:58:48 +0300

    feat:edd functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
+  if (array==0 || arguments.length==0){
   return 0;
 }
-
+else{
+  return Math.min.apply(null,array)
+}
+}
 exports.max = function max (array) {
-  return 0;
+  if (array==0 || arguments.length==0){
+    return 0;
+  }
+  else{
+    return Math.max.apply(null,array)
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==0 || arguments.length==0){
+    return 0;
+  }
+  else{
+    return (array.reduce(function(x,y){
+      return x+y
+    },0)/array.length)
+  }
 }
